fc322128357d7e8cbe44cd71bb76dc9e464f0348
jinno <klvx01@gmail.com>
2025-06-12 00:26:35 +0900

    feat: create IndexPage component with initial structure and meta tags

---
+++ apps/frontend/app/IndexPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>My Page</title>
+        {/* 他のメタタグやリンクタグを追加 */}
+      </head>
+      <body>
+        <h1>Welcome to my page</h1>
+        {/* コンテンツを追加 */}
+      </body>
+    </>
+  );
+};
+
+export default IndexPage;
