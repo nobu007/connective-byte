e5020a6be495b4bf65217865b072bcc40c858005
jinno <klvx01@gmail.com>
2025-06-26 01:07:40 +0900

    feat: initialize project with ESLint, testing and formatting setup

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from "globals";
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+
+export default [
+  { ignores: ["node_modules/", "**/.next/", ".windsurf/", "index.html"] },
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    files: ["apps/backend/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+];
