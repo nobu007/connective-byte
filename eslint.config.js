25865a6d5040cd889b76d34ea1044cb63b6bd626
jinno <klvx01@gmail.com>
2025-09-21 03:47:40 +0900

    build: ESLintとStylelintの設定を強化し、カバレッジファイルを無視するように変更
    ESLintの設定ファイルを更新し、不要なファイルを無視するパターンを追加
    Stylelintのコマンドに.gitignoreを参照し、カバレッジディレクトリを無視するオプションを追加
    これらの変更により、不要なファイルに対するリンターの実行を防ぎ、パフォーマンスを向上させる

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
 
 export default [
-  { ignores: ["node_modules/", "**/.next/", ".windsurf/", "index.html"] },
+  { ignores: ["node_modules/", "**/.next/", ".windsurf/", "index.html", "**/coverage/**", "**/*.config.js"] },
   js.configs.recommended,
   prettier,
   {
